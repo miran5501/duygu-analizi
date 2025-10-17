519d1cdf1635fef2e46d96231f905a018b806432
Eser Miran <esermiran1@gmail.com>
2025-10-18 00:53:35 +0300

    test yaptığımda tabloların bulunamama sorunu vardı onla alakalı program.cs düzeltildi

--- backend/duygu-analizi/duygu-analizi/Program.cs
+++ backend/duygu-analizi/duygu-analizi/Program.cs
@@ -45,4 +45,10 @@ app.UseCors("AllowAll");
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.Run();
