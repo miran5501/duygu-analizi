0b1964fe664575b30faa6375ce888b27502cbc00
Eser Miran <esermiran1@gmail.com>
2025-10-19 05:05:52 +0300

    Frontendde tarih dönüşümü yapmadığım için saatler 3 saat geri gözüküyor o işlemi düzeltmek için anlık çözüm olarak burada tarihe +3 saat ekliyorum ileride düzeltilecek.

--- backend/duygu-analizi/duygu-analizi/Models/Entities/Mesaj.cs
+++ backend/duygu-analizi/duygu-analizi/Models/Entities/Mesaj.cs
@@ -6,7 +6,9 @@
         public string Text { get; set; } = "";
         public string DuyguDurumu { get; set; } = "";
         public double DuyguYuzdesi { get; set; }
-        public DateTime OlusturulmaTarihi { get; set; } = DateTime.UtcNow;
+        //Frontendde tr-TR dönüşümü yapmadığım için tarihte 2 frontendi düzeltmekle uğraşmamak için anlık çözümde buradan 3 saat ekliyorum
+        //Frontendde o işlem düzeltildiğinde burada +3 saat ekleme kaldırılacak.
+        public DateTime OlusturulmaTarihi { get; set; } = DateTime.UtcNow.AddHours(3);
         public int KullaniciId { get; set; }
         public Kullanici? Kullanici { get; set; }
     }
